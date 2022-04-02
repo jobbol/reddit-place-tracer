300e2cd1566686a15b62f5a7043a9e442371a694
lobcog <102873247+lobcog@users.noreply.github.com>
2022-04-02 15:10:05 -0500

    Update userscript.user.js

--- userscript.user.js
+++ userscript.user.js
@@ -2,6 +2,7 @@
 // @name         /r/Place Enter the Gungeon template
 // @namespace    http://tampermonkey.net/
 // @version      1.2
+// @downloadURL  https://github.com/lobcog/reddit-place-tracer/raw/main/userscript.user.js
 // @description  Draws a template on top of the canvas showing where to click and what colors to use.  Does not do any clicking for you.
 // @author       lob, oralekin
 // @match        https://hot-potato.reddit.com/embed*
